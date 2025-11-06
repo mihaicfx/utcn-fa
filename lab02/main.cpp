a9e5d1a3ba381859cf0beaf0fbbb92f4cfc40f18
Mihai Feier <mihaifx@gmail.com>
2025-11-06 21:32:36 +0200

    add some convenience commands to labs 1-3

--- lab02/main.cpp
+++ lab02/main.cpp
@@ -8,6 +8,7 @@
 
 #include <cstdio>
 #include <string>
+#include <vector>
 
 using namespace lab02;
 
@@ -39,11 +40,55 @@ void bench(const CommandArgs& args)
     profiler.reset();
 }
 
+void print_array(const char* message, const std::vector<int>& values)
+{
+    printf("%s: ", message);
+    for (int value : values) {
+        printf("%d ", value);
+    }
+    printf("\n");
+}
+
+std::vector<int> read_array(const CommandArgs& args)
+{
+    std::vector<int> v;
+    for (const auto& arg : args) {
+        v.push_back(atoi(arg));
+    }
+    return v;
+}
+
+void demo_heap_build(const CommandArgs& args, void (*build_func)(int*, int, Operation*, Operation*))
+{
+    std::vector<int> v = read_array(args);
+
+    build_func(v.data(), static_cast<int>(v.size()), nullptr, nullptr);
+    print_array("Heap:", v);
+}
+
+void demo_heapsort(const CommandArgs& args) {
+    std::vector<int> v = read_array(args);
+
+    heapSort(v.data(), static_cast<int>(v.size()));
+    print_array("Sorted:", v);
+}
+
+void demo_bottomup(const CommandArgs& args) {
+    demo_heap_build(args, buildHeap_BottomUp);
+}
+
+void demo_topdown(const CommandArgs& args) {
+    demo_heap_build(args, buildHeap_TopDown);
+}
+
 int main()
 {
     const std::vector<CommandSpec> commands =
     {
         {"demo", demo, "run demo"},
+        {"heapsort", demo_heapsort, "[num1] [num2] ... - demonstrate Heap Sort"},
+        {"bottomup", demo_bottomup, "[num1] [num2] ... - demonstrate Bottom-Up Build-Heap"},
+        {"topdown", demo_topdown, "[num1] [num2] ... - demonstrate Top-Down Build-Heap"},
         {"test", test, "run unit-tests"},
         {"perf", perf, "[avg(default)|best|worst] - run performance analysis on selected case"},
         {"bench", bench, "[avg(default)|best|worst] - run benchmarks on selected case"},
